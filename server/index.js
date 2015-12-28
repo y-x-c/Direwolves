c1d71b3782b68450ec2ae2a6c77f36a23ec0c3a3
Orthocenter <chenyuxin.mail@gmail.com>
2015-12-29 00:34:14 +0800

    package server

--- server/index.js
+++ server/index.js
@@ -8,8 +8,32 @@
 global.clients = [];
 global.docs = {};
 
+const electron = require('electron');
+const electronApp = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
 var Socket = require('./socket');
 
 var socket = new Socket('0.0.0.0', '8888');
 
 socket.start();
+
+//////
+
+var mainWindow = null;
+
+electronApp.on('window-all-closed', function() {
+    client.end();
+    electronApp.quit();
+});
+
+electronApp.on('ready', function() {
+    global.mainWindow =  mainWindow = new BrowserWindow({width: 300, height: 180});
+
+    mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+    mainWindow.on('closed', function() {
+        mainWindow = null;
+    });
+});
+
